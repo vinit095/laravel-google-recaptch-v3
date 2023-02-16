8220154f250fb91f1618050e1af6f6b717328454
Vinit Soni <vinit095@gmail.com>
2023-02-16 13:29:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# About
+
+This project contain Google recaptcha v3. Used laravel breeze blade dark mode.
+It contains below client side approach.
+
+1. Automatically bind the challenge to a button
+2. Programmatically invoke the challenge.
+
+## USE
+
+Follow below steps:
+
+1. migrate database.
+2. create google recaptcha v3 on google developer account.
+3. Copy Site/Secret key in env under name of "GOOGLE_RECAPTCHA_SITE_KEY", "GOOGLE_RECAPTCHA_SECRET_KEY".
+4. Run 'npm install', 'php artisan serve', 'npm run dev' commands.
+5. Visit '/register' page.
---
+++ app/Rules/Recaptcha.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Recaptcha implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $gresponse = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => config('services.recaptcha.secret'),
+            'response' => $value,
+            'remoteip' => request()->ip()
+        ]);
+        if (!$gresponse->json('success')) {
+            $fail('The ' . $attribute . ' is invalid.');
+        }
+    }
+}
